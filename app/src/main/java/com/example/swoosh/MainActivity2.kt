574b5eee704107c8dff49bb2824c97d7f69c5416
Chandan <cks123373@gmail.com>
2020-09-27 21:04:24 +0530

    Initial commit

--- app/src/main/java/com/example/swoosh/MainActivity2.kt
+++ app/src/main/java/com/example/swoosh/MainActivity2.kt
@@ -2,10 +2,29 @@ package com.example.swoosh
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main2.*
 
 class MainActivity2 : AppCompatActivity() {
+    var checkedItem = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+    }
+
+    fun womenClicked(view : View){
+        menBtn.isChecked=false
+        coedBtn.isChecked=false
+        checkedItem ="women"
+    }
+    fun menClicked(view: View){
+        womenBtn.isChecked=false
+        coedBtn.isChecked=false
+        checkedItem ="men"
+    }
+    fun coedClicked(view: View){
+        menBtn.isChecked=false
+        womenBtn.isChecked=false
+        checkedItem ="coed"
     }
 }
